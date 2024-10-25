f6683e654d31bb20d6e766fbb970abcf21540d62
circustar <13661947633@163.com>
2024-10-25 20:02:15 +0800

    release 1.2.5 hotfix1

--- src/main/java/com/circustar/mybatis_accessor/injector/methods/SelectBatchIdsWithJoin.java
+++ src/main/java/com/circustar/mybatis_accessor/injector/methods/SelectBatchIdsWithJoin.java
@@ -2,6 +2,9 @@ package com.circustar.mybatis_accessor.injector.methods;
 
 public class SelectBatchIdsWithJoin extends SelectListWithJoin {
     private static final long serialVersionUID = -1L;
+    public SelectBatchIdsWithJoin() {
+        super("SelectBatchIdsWithJoin");
+    }
     @Override
     protected CSSqlMethod getSqlMethod() {
         return CSSqlMethod.SELECT_BATCH_BY_IDS_WITH_JOIN;
